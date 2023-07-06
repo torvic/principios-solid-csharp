8670ef1fbd8a711001ce2cde48ceeba558ad0b55
torvic <torvic02st96@gmail.com>
2023-07-05 22:45:56 -0500

    Initial commit

---
+++ 1-SingleResponsability/Program.cs
@@ -0,0 +1,7 @@
+﻿using SingleResponsability;
+
+StudentRepository studentRepository = new();
+ExportHelper exportHelper = new();
+
+exportHelper.ExportStudents(studentRepository.GetAll());
+Console.WriteLine("Proceso Completado");
